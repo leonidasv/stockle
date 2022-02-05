8f40dc2cb7d2fe2df1391f8d2da73a388d31da36
Leonidas Villeneuve <leonidas@leonidasv.com>
2022-02-05 18:32:01 -0300

    tooltip dark mode improvement

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -10,7 +10,7 @@ export function Tooltip({ id, children }: Props) {
     <div
       id={id}
       role="tooltip"
-      className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700 z-50"
+      className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-200 dark:text-gray-700 z-50"
     >
       {children}
       <div className="tooltip-arrow" data-popper-arrow="" />
